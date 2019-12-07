2c764f5a3a946397df408931ee3e28e70a0e1bfa
quan8 <quan.nguyen@fantom.foundation>
2019-12-08 05:56:51 +1100

    dot diff

---
+++ bin/dotdiff.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# compare the dot output files
+N=$1
+dir=./lachesis_images
+FILE=DAG-EPOCH-1
+
+diffdir=${dir}/diff
+mkdir -p ${diffdir}
+
+for i in $(seq $((N-1)))
+do
+	echo $i
+	f=${dir}/$i/${FILE}
+	fo=${dir}/$((i+1))/${FILE}	
+	difffile="${diffdir}/$i-$((i+1))".diff
+	
+	diff ${f}.dot ${fo}.dot > ${difffile}
+done
+
